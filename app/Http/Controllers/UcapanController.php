dc8dbf4cae39312754e8bd659da7d71e3111a0fc
Nando WItin <108390976+nandowitin0602@users.noreply.github.com>
2025-01-24 20:11:13 +0700

    Fixing Bug

--- app/Http/Controllers/UcapanController.php
+++ app/Http/Controllers/UcapanController.php
@@ -16,12 +16,125 @@ class UcapanController extends Controller
             'konfirmasi_kehadiran' => 'required|string',
         ]);
 
+        // Daftar nama yang Valid
+        $validNames = [
+            'Loretta',
+            'Pricilia Rey',
+            'Yoga Wijaya & Partner',
+            'Fendi & Keluarga',
+            'Agung Sanjaya & Istri',
+            'Dedi Suryadi & Keluarga',
+            'Muliono Rudy & Keluarga',
+            'Jan Rahanra & keluarga',
+            'Edi Oilira & keluarga',
+            'Cut Mega',
+            'Diah Nurul Fajrina',
+            'Simon Liu & Keluarga',
+            'Liu Se Lim & Keluarga',
+            'Liu Hie Lim',
+            'Sally',
+            'Selvi Angelia',
+            'Prasetya Setiawan & Family',
+            'Henry Denata & Family',
+            'Emi Suherna & Family',
+            'Lenny Adelina & Partner',
+            'Roselina & Partner',
+            'Halimah & Keluarga',
+            'Ci Lianna',
+            'Juneike',
+            'Indah Fatma',
+            'Cynthia',
+            'Dewi Purnamasari',
+            'Frisca lychia & Partner',
+            'Hengky & Keluarga',
+            'Mega',
+            'Nita Caroline',
+            'Sarah Devina & Keluarga',
+            'Stephanie',
+            'Welly',
+            'Yefieka Liestiyani',
+            'Yohanes C Tanjung & Partner',
+            'Cindy Lie & Keluarga',
+            'Nicky',
+            'Elyanti & Keluarga',
+            'Rifky Wilantara & Hartina Naga Wijaya',
+            'Putu Gita Andika & Harlina Naga Wijaya',
+            'Gunadi Naga Wijaya & Partner',
+            'Haryani Naga Wijaya & Partner',
+            'Muhammad Mustofa & Keluarga',
+            'William Linandry Wijaya & Keluarga',
+            'Herlina & Partner',
+            'Ivan',
+            'Andreano Lieonard & Keluarga',
+            'Rendy Leonardo & Keluarga',
+            'Vanesa Moe & Keluarga',
+            'Andrianto Handojo & Keluarga',
+            'Pavita Priscilia & Keluarga',
+            'Metta Lastiawani & Keluarga',
+            'Jufeity Kosasih',
+            'Fendy Tjie',
+            'Andy Daniswara',
+            'Raka Al Fajri',
+            'Sisielius Yuan Reynaldo',
+            'Ardiawan Kurniandi & Keluarga',
+            'Santo Hura',
+            'Ryan Putra & Partner',
+            'Owen Putra Wijaya & Keluarga',
+            'Kendry Juwono & Keluarga',
+            'Steven Liao & Partner',
+            'Patrick Fernando Ciputra & Partner',
+            'Christopher Jeremy & Partner',
+            'Oktavianus Kurniawan & Keluarga',
+            'Andrean & Partner',
+            'Nico Liu & Marco Nicholas',
+            'Willy Leonardi & partner',
+            'Yucia Juniar & Partner',
+            'William Manopo & Keluarga',
+            'Rudy Ming & Keluarga',
+            'Deddy Sanjaya & Partner',
+            'Dyana Septeani',
+            'Rendy Purnama & Keluarga',
+            'Handika & Keluarga',
+            'Wirananda Eka Wijaya & Partner',
+            'Novi & Partner',
+            'Ronny',
+            'Jessica Hartono',
+            'Ko Irwan & Keluarga',
+            'Fredy Onggo & Partner',
+            'Visarada & Partner',
+            'Evirna Ester & Partner',
+            'Jessica Wijaya & Partner',
+            'PT Pejuang Senyum Abadi',
+            'Alfred',
+            'Nita',
+            'Steven & Elvia',
+            'Daniel Budi & partner',
+            'Ricky & Linsye',
+            'Vincent & Gresyeila',
+            'Pedro',
+            'Lydia Maheswari',
+            'Sunaryo',
+            'Aryo & Partner',
+            'Fendy & Partner',
+            'Feber',
+            'Vera & Partner',
+            'Sulli'
+        ];
+
+        // Trim dan lowercase setiap elemen dalam validNames
+        $validNames = array_map(function ($name) {
+            return strtolower(trim($name));
+        }, $validNames);
+
+        // Jika nama tidak ada di array validNames, maka diisi "Tamu Undangan"
+        $nama = in_array(strtolower(trim($validated['nama'])), $validNames) ? $validated['nama'] : 'Tamu Undangan';
+
         try {
             DB::insert("
-                INSERT INTO ucapan (Nama, Ucapan, Konfirmasi_Kehadiran, Addtime)
-                VALUES (?, ?, ?, NOW())
-            ", [
-                $validated['nama'],
+            INSERT INTO ucapan (Nama, Ucapan, Konfirmasi_Kehadiran, Addtime)
+            VALUES (?, ?, ?, NOW())
+        ", [
+                $nama,
                 $validated['ucapan'],
                 $validated['konfirmasi_kehadiran']
             ]);
